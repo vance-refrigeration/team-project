0fb879504061a51190e19ee61642f32b14eabcc7
colekins <colekins@gmail.com>
2017-11-07 11:31:15 -0500

    Add to cart controller action returns JSON
    - Altered add to cart action to return user object as JSON

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -116,9 +116,13 @@ const addproduct = (req, res, next) => {
   }).then(function (user) {
     user.cart.push(req.body.product)
     return user.save()
-  }).then((/* user */) =>
-    res.sendStatus(204)
-  ).catch(makeErrorHandler(res, next))
+  }).then(user => {
+    user = user.toObject()
+    delete user.passwordDigest
+    user.token = encodeToken(user.token)
+    res.json({ user })
+  })
+    .catch(makeErrorHandler(res, next))
 }
 
 const removeproduct = (req, res, next) => {
